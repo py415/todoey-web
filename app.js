e56e1946350feb1d75e37eaf548ec49a934a6290
py415 <philipyu1997@gmail.com>
2021-04-23 23:13:31 -0700

    Add templates and basic todo list form

--- app.js
+++ app.js
@@ -3,9 +3,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let items = []
+
+// Set view engine to EJS
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: true}))
+
 // GET home route
 app.get('/', (req, res) => {
-    res.send('Server is up and running.')
+    // Find todays date
+    let today = new Date()
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+    let day = today.toLocaleDateString('en-US', options)
+
+    // Render todays date
+    res.render('pages/list', {kindOfDay: day, newListItems: items})
+})
+
+// POST home route
+app.post('/', (req, res) => {
+    // Fetch newItem from text input field
+    let item = req.body.newItem
+
+    items.push(item)
+
+    // Redirect page back to home route upon post request
+    res.redirect('/')
 })
 
 // Listen for app on localhost port
