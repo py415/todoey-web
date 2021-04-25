0c5ba30be5ae11c7eb8af16f869ca034b21d8f77
py415 <philipyu1997@gmail.com>
2021-04-25 16:42:37 -0700

    Add custom routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Setup dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 const app = express();
 const port = 3000;
@@ -16,7 +17,9 @@ mongoose.connect("mongodb://localhost:27017/todolistDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.set("useFindAndModify", false);
 
+// Setup item schema
 const itemSchema = mongoose.Schema({
   name: String,
 });
@@ -35,18 +38,26 @@ const item3 = new Item({
   name: "Finish homework",
 });
 
-const itemList = [item1, item2, item3];
+const defaultItems = [item1, item2, item3];
+
+// Setup list schema
+const listSchema = mongoose.Schema({
+  name: String,
+  items: [itemSchema],
+});
+
+const List = mongoose.model("List", listSchema);
+
+let day = date.getDate();
 
 // GET home route
 app.get("/", (req, res) => {
-  let day = date.getDate();
-
   // Find all items in database
   Item.find({}, function (err, foundItems) {
     // Check if item database is empty
     if (foundItems.length === 0) {
       // Insert default items
-      Item.insertMany(itemList, function (err, item) {
+      Item.insertMany(defaultItems, function (err, item) {
         if (err) {
           console.log(err);
         } else {
@@ -61,41 +72,84 @@ app.get("/", (req, res) => {
   });
 });
 
+// GET customListName route
+app.get("/:customListName", (req, res) => {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({ name: customListName }, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        // Create new custom list name route
+        const list = List({
+          name: customListName,
+          items: defaultItems,
+        });
+
+        list.save(function (err, result) {
+          res.redirect("/" + customListName);
+        });
+      } else {
+        // Render custom name page
+        res.render("pages/list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
+});
+
 // POST home route
 app.post("/", (req, res) => {
   // Fetch newItem from text input field
   let itemName = req.body.newItem;
+  let listName = req.body.list;
 
   // Create new to do list item
   const item = Item({
     name: itemName,
   });
 
-  // Add new item to mongoDB
-  item.save();
-
-  // Redirect page back to home route to show updated to do list
-  res.redirect("/");
+  if (listName === day) {
+    // Add new item to mongoDB
+    item.save(function (err, result) {
+      // Redirect page back to home route to show updated to do list
+      res.redirect("/");
+    });
+  } else {
+    List.findOne({ name: listName }, (err, foundList) => {
+      foundList.items.push(item);
+      foundList.save(function (err, result) {
+        res.redirect("/" + foundList.name);
+      });
+    });
+  }
 });
 
 // POST delete route
 app.post("/delete", (req, res) => {
   // Find item id connected to checkbox
-  const checkboxId = req.body.checkbox;
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
 
-  Item.findByIdAndDelete(checkboxId, function (err, item) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Deleted: ", item);
-      res.redirect("/");
-    }
-  });
-});
-
-// GET work route
-app.get("/work", (req, res) => {
-  res.render("pages/list", { listTitle: "Work", newListItems: workItems });
+  if (listName == day) {
+    Item.findByIdAndDelete(checkedItemId, function (err, item) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Deleted: ", item);
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      (err, list) => {
+        res.redirect("/" + listName);
+      }
+    );
+  }
 });
 
 // Listen for app on localhost port
