7d78a90175923b22356334eeaa0f7fb005db803b
py415 <philipyu1997@gmail.com>
2021-04-25 15:18:00 -0700

    Setup mongoDB and add remove and add item functionality

--- app.js
+++ app.js
@@ -1,47 +1,104 @@
 // Setup dependencies
-const express = require('express')
-const date = require(__dirname + '/date.js')
-const app = express()
-const port = 3000
-
-const items = []
-const workItems = []
+const express = require("express");
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
+const app = express();
+const port = 3000;
 
 // Set view engine to EJS
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Setup mongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemSchema = mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Buy eggs",
+});
+
+const item2 = new Item({
+  name: "Buy milk",
+});
+
+const item3 = new Item({
+  name: "Finish homework",
+});
+
+const itemList = [item1, item2, item3];
 
 // GET home route
-app.get('/', (req, res) => {
-    
-    let day = date.getDate()
+app.get("/", (req, res) => {
+  let day = date.getDate();
 
-    // Render todays date
-    res.render('pages/list', {listTitle: day, newListItems: items})
-})
+  // Find all items in database
+  Item.find({}, function (err, foundItems) {
+    // Check if item database is empty
+    if (foundItems.length === 0) {
+      // Insert default items
+      Item.insertMany(itemList, function (err, item) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(item);
+        }
+      });
+
+      res.redirect("/");
+    } else {
+      res.render("pages/list", { listTitle: day, newListItems: foundItems });
+    }
+  });
+});
 
 // POST home route
-app.post('/', (req, res) => {
-    // Fetch newItem from text input field
-    let item = req.body.newItem
+app.post("/", (req, res) => {
+  // Fetch newItem from text input field
+  let itemName = req.body.newItem;
 
-    if (req.body.list === 'Work') {
-        workItems.push(item)
-        res.redirect('/work')
+  // Create new to do list item
+  const item = Item({
+    name: itemName,
+  });
+
+  // Add new item to mongoDB
+  item.save();
+
+  // Redirect page back to home route to show updated to do list
+  res.redirect("/");
+});
+
+// POST delete route
+app.post("/delete", (req, res) => {
+  // Find item id connected to checkbox
+  const checkboxId = req.body.checkbox;
+
+  Item.findByIdAndDelete(checkboxId, function (err, item) {
+    if (err) {
+      console.log(err);
     } else {
-        items.push(item)
-        res.redirect('/')
+      console.log("Deleted: ", item);
+      res.redirect("/");
     }
-})
+  });
+});
 
 // GET work route
-app.get('/work', (req, res) => {
-    res.render('pages/list', {listTitle: 'Work', newListItems: workItems})
-})
+app.get("/work", (req, res) => {
+  res.render("pages/list", { listTitle: "Work", newListItems: workItems });
+});
 
 // Listen for app on localhost port
 app.listen(port, () => {
-    console.log('Server is running on port', port)
-})
+  console.log("Server is running on port", port);
+});
